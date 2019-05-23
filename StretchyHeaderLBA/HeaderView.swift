98e58dbea0eefd970a34f12b93a511584ec2e206
vattanac <vattanacsim99@gmail.com>
2019-05-23 21:50:21 +0700

    ep4

--- StretchyHeaderLBA/HeaderView.swift
+++ StretchyHeaderLBA/HeaderView.swift
@@ -25,7 +25,43 @@ class HeaderView: UICollectionReusableView {
         
         //blur
         setupVisualEffectBlur()
+        
+        //Gradient
+        setupGradientLayer()
     }
+    
+    fileprivate func setupGradientLayer() {
+        let gradienLayer = CAGradientLayer()
+        gradienLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradienLayer.locations = [0.5, 1]
+        
+        //layer.addSublayer(gradienLayer)
+        let gradientContainerView = UIView()
+        addSubview(gradientContainerView)
+        gradientContainerView.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
+        
+        gradientContainerView.layer.addSublayer(gradienLayer)
+        gradienLayer.frame = self.bounds
+        gradienLayer.frame.origin.y -= bounds.height
+        
+        let heavyLabel = UILabel()
+        heavyLabel.text = "Surf the web for courses"
+        heavyLabel.font = .systemFont(ofSize: 24, weight: .heavy)
+        heavyLabel.textColor = .white
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Go ont the website khmer to learn iOS everyday for everyone it's free for all of you come and get it"
+        descriptionLabel.font = .systemFont(ofSize: 12, weight: .regular)
+        descriptionLabel.textColor = .white
+        descriptionLabel.numberOfLines = 0
+        
+        let stackView = UIStackView(arrangedSubviews: [heavyLabel, descriptionLabel])
+        stackView.axis = .vertical
+        
+        addSubview(stackView)
+        stackView.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 16, bottom: 16, right: 16))
+    }
+    
     var animator: UIViewPropertyAnimator!
     
     fileprivate func setupVisualEffectBlur() {
@@ -37,8 +73,12 @@ class HeaderView: UICollectionReusableView {
             self?.addSubview(visualEffectView)
             visualEffectView.fillSuperview()
         })
+        
+        animator.fractionComplete = 0
        //animator.startAnimation()
     }
+    
+    
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
